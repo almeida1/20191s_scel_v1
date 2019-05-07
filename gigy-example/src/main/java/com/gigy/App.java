0e35b60774b104f8e30864acfca69172585b91d5
professor <professor@L1MP.IPIRANGA.FATEC>
2019-05-07 20:46:41 -0300

    versao inicial

---
+++ gigy-example/src/main/java/com/gigy/App.java
@@ -0,0 +1,13 @@
+package com.gigy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://gigsterous.github.io/engineering/2017/03/01/spring-boot-4.html
+//exemplo de teste utilizando when drinkingbuddyservice
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
